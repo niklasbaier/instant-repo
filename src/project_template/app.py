3ab5d76babfc78c0794128a8c69129a136db5a3a
niklasbaier <niklas.baier@yahoo.de>
2024-05-10 13:57:09 +0200

    feat: add terminal & file logging for dev & prod environments

--- src/project_template/app.py
+++ src/project_template/app.py
@@ -1,17 +1,18 @@
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+from commons import logging_helpers
 
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s, [%(levelname)s] in %(name)s: %(message)s")
 
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+def main():
+    logging_helpers.setup_logging()
+    logger = logging.getLogger(__name__)
 
-if __name__ == "__main__":
     logger.debug("This is a log entry for debugging.")
     logger.info("This is a log entry for information.")
     logger.warning("This is a log entry for a warning.")
     logger.error("This is a log entry for an error.")
     logger.critical("This is a log entry for a critical error.")
+
+
+if __name__ == "__main__":
+    main()
